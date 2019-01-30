b4ef15fe549e9edc6d95f006592b438c53d8c335
gracekroeger <grkr1069@colorado.edu>
2019-01-29 21:46:42 -0700

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,7 +1,6 @@
 $(document).ready(function() { // do this when the document is loaded
 	$("#dialerContent").show(); // show the element with ID "element"
 	$("#conatctContent").hide();
-	$("#addContent").hide();// hide the element with ID "otherElement"
 });
 
 $("#button_id").click(function() { // when "button_id" is clicked
