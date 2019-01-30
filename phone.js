e359dc46f0693a4f6ed1ab468143477407f64455
gracekroeger <grkr1069@colorado.edu>
2019-01-30 13:36:36 -0700

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,7 +1,25 @@
-
 $(document).ready(function() {
-	$("#dialercontent").show(); 
-	$("#conatctcontent").hide();
+	$("#dialerContent").show(); 
+	$("#contactContent").hide();
+	$("#addContent").hide();
+});
+
+$("#dialerName").click(function() { // when "button_id" is clicked
+	$("#dialerContent").show(); // show element
+	$("#contactContent").hide();
+	$("#addContent").hide();
+});
+
+$("#contactName").click(function() { // when "button_id" is clicked
+	$("#contactContent").show(); // show element
+	$("#dialerContent").hide();
+	$("#addContent").hide();
+});
+
+$("#addName").click(function() { // when "button_id" is clicked
+	$("#addContent").show(); // show element
+	$("#dialerContent").hide();
+	$("#contactContent").hide();
 });
 
 
