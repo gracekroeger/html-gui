f018f6587ebbb86d2ce6917b2a7d53f3f866b749
gracekroeger <grkr1069@colorado.edu>
2019-01-29 21:48:45 -0700

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,9 +1,6 @@
 $(document).ready(function() { // do this when the document is loaded
-	$("#dialerContent").show(); // show the element with ID "element"
-	$("#conatctContent").hide();
+	$("#dialercontent").show(); // show the element with ID "element"
+	$("#conatctcontent").hide();
 });
 
-$("#button_id").click(function() { // when "button_id" is clicked
-	$("#element").show(); // show element
-	$("#other_element").hide();	// hide other element
-});
+
